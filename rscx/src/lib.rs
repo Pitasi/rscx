b00fb173857290d22a7d8f3fea763e5d3f417d64
Antonio Pitasi <antonio@pitasi.dev>
2023-09-09 20:02:31 +0200

    add collect_fragment_async() helper method

--- rscx/src/lib.rs
+++ rscx/src/lib.rs
@@ -3,11 +3,16 @@ pub mod axum;
 pub mod context;
 
 pub extern crate rscx_macros;
+use std::future::Future;
+
 pub use rscx_macros::*;
 
 pub extern crate typed_builder;
 
 pub extern crate html_escape;
+
+use async_trait::async_trait;
+use futures::future::join_all;
 
 pub trait CollectFragment<I>
 where
@@ -24,5 +29,27 @@ where
 {
     fn collect_fragment(self) -> String {
         self.collect::<Vec<String>>().join("")
+    }
+}
+
+#[async_trait]
+pub trait CollectFragmentAsync<I, Fut>
+where
+    I: Iterator,
+    Vec<Fut>: FromIterator<<I as Iterator>::Item>,
+    Fut: Future<Output = String>,
+{
+    async fn collect_fragment_async(self) -> String;
+}
+
+#[async_trait]
+impl<I, Fut> CollectFragmentAsync<I, Fut> for I
+where
+    I: Iterator + Send,
+    Vec<Fut>: FromIterator<<I as Iterator>::Item>,
+    Fut: Future<Output = String> + Send,
+{
+    async fn collect_fragment_async(self) -> String {
+        join_all(self.collect::<Vec<_>>()).await.join("")
     }
 }
